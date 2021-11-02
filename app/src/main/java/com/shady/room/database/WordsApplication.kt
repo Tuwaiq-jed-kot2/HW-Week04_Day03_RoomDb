8a868861735f69091be4a98f428a5bb3e11b99e1
RajwaAbdulaziz <Rajwa.Abdulaziz.jed.k2@tuwaiq.edu.sa>
2021-11-02 22:39:18 +0300

    solved

---
+++ app/src/main/java/com/shady/room/database/WordsApplication.kt
@@ -0,0 +1,15 @@
+package com.shady.room.database
+
+import android.app.Application
+import com.shady.room.database.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+
+class WordsApplication : Application() {
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
