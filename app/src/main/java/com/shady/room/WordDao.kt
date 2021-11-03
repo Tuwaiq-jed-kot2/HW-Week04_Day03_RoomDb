c5638da1b5eef1773c408d57c5dc9611d01d7791
bashayerH <Bashayer.Ismaeel.jed.k2@tuwaiq.edu.sa>
2021-11-03 08:51:41 +0300

    there is a lot of ERROR -_-

---
+++ app/src/main/java/com/shady/room/WordDao.kt
@@ -0,0 +1,21 @@
+package com.shady.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface WordDao {
+
+    @Query("SELECT * FROM word_table OEDER BY word ASC")
+    fun getAlphabetizedWord(): Flow<List<Word>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(word: Word)
+
+    @Query("DELETE FROM word_table")
+    suspend fun deleteAll()
+}
